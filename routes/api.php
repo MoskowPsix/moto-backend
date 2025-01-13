5dbaf7656a95837157985f2c644f0b7b82007fd6
MoskowPsix <bolshe.kivi@gmail.com>
2025-01-13 18:09:05 +0500

    init

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->name('user.login');
-
     Route::post('register', 'register')->name('user.register');
     Route::post('logout', 'logout')->middleware('auth:sanctum')->name('user.logout');
 });
