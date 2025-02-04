f97837f559c19b59bddcb07ba90c3658530c251c
MoskowPsix <bolshe.kivi@gmail.com>
2025-02-04 20:38:07 +0500

    add: middleware for role

---
+++ app/Http/Middleware/VerificationEmailMiddlaware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Resources\Error\NotAuthUserResource;
+use App\Http\Resources\Error\NotVerificationEmailResouece;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerificationEmailMiddlaware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response|NotVerificationEmailResouece|NotAuthUserResource
+    {
+        if (empty($request->user())) {
+            return NotAuthUserResource::make([]);
+        }
+        if (!$request->user()->hasVerifiedEmail()) {
+            return NotVerificationEmailResouece::make([]);
+        }
+        return $next($request);
+    }
+}
