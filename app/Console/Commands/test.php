ca476e069cef3c710b1c127bdb6ef3ed3942f0ae
MoskowPsix <bolshe.kivi@gmail.com>
2025-03-10 18:58:45 +0500

    merge develop 10.03.2024

--- app/Console/Commands/test.php
+++ app/Console/Commands/test.php
@@ -31,57 +31,56 @@ class test extends Command
      */
     public function handle()
     {
-        $login = env('ROBOKASSA_LOGIN');
-        $password = env('ROBOKASSA_TEST_PASSWORD1');
-
-        $invoiceId = 678678;
-        $description = 'Услуга от 500';
-        $outSum = 500;
-        $IsTest = 1;
-
-        $crc = md5("$login:$outSum:$invoiceId:$password");
-
-        $link = "https://auth.robokassa.ru/Merchant/Index.aspx?" .
-            http_build_query([
-                'MrchLogin' => $login,
-                'OutSum' => $outSum,
-                'InvId' => $invoiceId,
-                'Desc' => $description,
-                'SignatureValue' => strtoupper($crc),
-                'IsTest' => $IsTest,
-            ]);
-        $this->info("Перейдите по следующей ссылке для тестовой оплаты:");
-        $this->line($link);
-
-//        Document::all()->each(function ($apps) {
-//            $data = json_decode($apps->data, true);
+//        $login = env('ROBOKASSA_LOGIN');
+//        $password = env('ROBOKASSA_TEST_PASSWORD1');
 //
-//            if (isset($data)) {
-//                if($apps->type->value === DocumentType::Polis->value) {
-//                    $apps->update([
-//                        'type' => 'polis',
-//                        'url_view' => $data['polisFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
-//                        'number' => $data['polisNumber'] ?? '',
-//                        'issued_whom' => $data['polisIssuedWhom'] ?? '',
-//                        'it_works_date' => $data['polisItWorksDate'] ?? '',
-//                    ]);
-//                }
-//                if($apps->type->value === DocumentType::Licenses->value) {
-//                    $apps->update([
-//                        'type' => 'licenses',
-//                        'url_view' => $data['licensesFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
-//                        'number' => $data['licensesNumber'] ?? '',
-//                    ]);
-//                }
-//                if($apps->type->value === DocumentType::Notarius->value) {
-//                    $apps->update([
-//                        'type' => 'notarius',
-//                        'url_view' => $data['notariusFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
-//                    ]);
-//                }
-//            }
-//        });
-//        return 1;
-//    }
+//        $invoiceId = 678678;
+//        $description = 'Услуга от 500';
+//        $outSum = 500;
+//        $IsTest = 1;
+//
+//        $crc = md5("$login:$outSum:$invoiceId:$password");
+//
+//        $link = "https://auth.robokassa.ru/Merchant/Index.aspx?" .
+//            http_build_query([
+//                'MrchLogin' => $login,
+//                'OutSum' => $outSum,
+//                'InvId' => $invoiceId,
+//                'Desc' => $description,
+//                'SignatureValue' => strtoupper($crc),
+//                'IsTest' => $IsTest,
+//            ]);
+//        $this->info("Перейдите по следующей ссылке для тестовой оплаты:");
+//        $this->line($link);
+
+        Document::all()->each(function ($apps) {
+            $data = json_decode($apps->data, true);
+
+            if (isset($data)) {
+                if($apps->type->value === DocumentType::Polis->value) {
+                    $apps->update([
+                        'type' => 'polis',
+                        'url_view' => $data['polisFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
+                        'number' => $data['polisNumber'] ?? '',
+                        'issued_whom' => $data['polisIssuedWhom'] ?? '',
+                        'it_works_date' => $data['polisItWorksDate'] ?? '',
+                    ]);
+                }
+                if($apps->type->value === DocumentType::Licenses->value) {
+                    $apps->update([
+                        'type' => 'licenses',
+                        'url_view' => $data['licensesFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
+                        'number' => $data['licensesNumber'] ?? '',
+                    ]);
+                }
+                if($apps->type->value === DocumentType::Notarius->value) {
+                    $apps->update([
+                        'type' => 'notarius',
+                        'url_view' => $data['notariusFileLink'] ?? 'https://dev-moto.vokrug.city/document/' . $apps->id,
+                    ]);
+                }
+            }
+        });
+        return 1;
     }
 }
