65b4811eedbb399f3073e5e3196a308bc37c1038
Makswer <makswolk6@yandex.ru>
2025-03-14 15:39:21 +0300

    fix: remove action in ActionServiceProvider

--- app/Providers/ActionServiceProvider.php
+++ app/Providers/ActionServiceProvider.php
@@ -71,8 +71,6 @@ class ActionServiceProvider extends ServiceProvider
         \App\Contracts\Actions\Controllers\Attendance\DeleteAttendanceActionContract::class                         => \App\Actions\Controllers\Attendance\DeleteAttendanceAction::class,
         \App\Contracts\Actions\Controllers\Transaction\CreateTransactionActionContract::class                       => \App\Actions\Controllers\Transaction\CreateTransactionAction::class,
         \App\Contracts\Actions\Controllers\Transaction\ResultTransactionActionContract::class                       => \App\Actions\Controllers\Transaction\ResultTransactionAction::class,
-        \App\Contracts\Actions\Controllers\Transaction\SuccessTransactionActionContract::class                      => \App\Actions\Controllers\Transaction\SuccessTransactionAction::class,
-
     ];
     /**
      * Register services.
