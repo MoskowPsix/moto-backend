470ec85f580c825a87052d31a982435dd6cd334e
MoskowPsix <bolshe.kivi@gmail.com>
2025-03-17 15:24:57 +0500

    add: add sort field in race

---
+++ app/Http/Resources/Errors/NotVerificationPhoneResouece.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Errors;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NotVerificationPhoneResouece extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'status' => 'error',
+            'message' => __('messages.error.not_verification_phone'),
+        ];
+    }
+    public function withResponse($request, $response): void
+    {
+        $response->setStatusCode(403);
+    }
+}
