5284502803ee9a314143949479d2e253ce88b052
Makswer <makswolk6@yandex.ru>
2025-03-19 10:36:22 +0300

    fix: add id in status resource

--- app/Http/Resources/Status/StatusResource.php
+++ app/Http/Resources/Status/StatusResource.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
+ * @property integer $id
  * @property string $name
  */
 class StatusResource extends JsonResource
@@ -17,6 +18,7 @@ class StatusResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'   => $this->id,
             'name' => $this->name,
         ];
     }
