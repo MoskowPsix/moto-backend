ee3bb23e086cb9c1891d85a6bc1d58d530159c35
Makswer <makswolk6@yandex.ru>
2025-04-07 14:09:51 +0300

    fix: fix bugs in test

--- tests/Feature/PersonalInfo/UpdatePersonalInfoActionTest.php
+++ tests/Feature/PersonalInfo/UpdatePersonalInfoActionTest.php
@@ -63,4 +63,45 @@ class UpdatePersonalInfoActionTest extends TestCase
 
         $this->assertInstanceOf(SuccessUpdatePersonalInfoResource::class, $response);
     }
+    public function test_action_not_found(): void
+    {
+        $firstUser = User::factory()->create();
+        $secondUser = User::factory()->create();
+        $command = Command::factory()->create();
+
+        $personalInfoSeed = PersonalInfo::factory()->create([
+            'user_id' => $firstUser->id,
+            'command_id' => $command->id,
+        ]);
+
+        $personalInfo = [
+            'name'              => $personalInfoSeed->name,
+            'surname'           => $personalInfoSeed->surname,
+            'patronymic'        => $personalInfoSeed->patronymic,
+            'date_of_birth'     => $personalInfoSeed->dateOfBirth,
+            'city'              => $personalInfoSeed->city,
+            'inn'               => $personalInfoSeed->inn,
+            'snils'             => $personalInfoSeed->snils,
+            'phone_number'      => $personalInfoSeed->phoneNumber,
+            'start_number'      => $personalInfoSeed->startNumber,
+            'group'             => $personalInfoSeed->group,
+            'rank_number'       => $personalInfoSeed->rankNumber,
+            'rank'              => $personalInfoSeed->rank,
+            'community'         => $personalInfoSeed->community,
+            'coach'             => $personalInfoSeed->coach,
+            'moto_stamp'        => $personalInfoSeed->motoStamp,
+            'engine'            => $personalInfoSeed->engine,
+            'user_id'           => $firstUser->id,
+            'region'            => $personalInfoSeed->region,
+            'number_and_seria'  => $personalInfoSeed->numberAndSeria,
+            'locationId'        => $personalInfoSeed->locationId,
+            'commandId'         => $command->id,
+        ];
+        Sanctum::actingAs($secondUser);
+        $request = new UpdatePersonalInfoRequest($personalInfo);
+        $action = app(UpdatePersonalInfoActionContract::class);
+        $response = $action($request);
+
+        $this->assertInstanceOf(NotFoundResource ::class, $response);
+    }
 }
