c12511f70e4340550b63c434ecc7d869e4b5bfc6
Makswer <makswolk6@yandex.ru>
2025-04-11 15:24:52 +0300

    add: delete sync commission if null in AddCommissionAction.php

--- app/Actions/Controllers/Race/AddCommissionAction.php
+++ app/Actions/Controllers/Race/AddCommissionAction.php
@@ -24,6 +24,14 @@ class AddCommissionAction implements AddCommissionActionContract
         if($race->user_id !== auth()->user()->id) {
             return NotUserPermissionResource::make([]);
         }
+
+        if(empty($request->usersIds)){
+            $race->commissions()->sync([]);
+            return SuccessAddCommissionResource::make([
+                'message'  => 'Комиссии удалены',
+            ]);
+        }
+
         User::whereIn('id', $request->usersIds)->each(function (User $user) {
             if(!$user->hasRole(RoleConstant::COMMISSION)){
                 $this->check_role = false;
