b9cfd62d959f383e5360504da6339908850f82e4
Makswer <makswolk6@yandex.ru>
2025-04-22 10:05:17 +0300

    add:

--- app/MoonShine/Resources/UserResource.php
+++ app/MoonShine/Resources/UserResource.php
@@ -262,7 +262,6 @@ class UserResource extends ModelResource
 
             Number::make('ID', 'id'),
             Text::make('Username', 'name'),
-            Phone::make('Phone', 'phone.number'),
             Email::make('E-mail', 'email'),
         ];
     }
