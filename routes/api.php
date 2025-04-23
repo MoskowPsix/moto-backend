5f2b2073545c37253fd21c001c52fb4b5b478508
Makswer <makswolk6@yandex.ru>
2025-04-23 10:43:55 +0300

    fiz: api appointment_race.export refactor

--- routes/api.php
+++ routes/api.php
@@ -146,8 +146,9 @@ Route::controller(App\Http\Controllers\Api\AppointmentRaceController::class)->gr
     Route::get('races/{id}/appointment-race/appointments', 'getAppointmentsUsers')
         ->middleware(['auth:sanctum', 'role:' . '|' . $role::COMMISSION .'|' .$role::ADMIN.'|'.$role::ROOT])
         ->name('appointment_race.get_users_table_appointment_race');
-
-    Route::get('races/{id}/appointment-race/users-table/export', 'export')->middleware(['auth:sanctum', 'role:' . $role::COMMISSION . '|' . $role::ORGANIZATION .'|' .$role::ADMIN.'|'.$role::ROOT])->name('appointment_race.export');
+    Route::get('races/{id}/appointment-race/users-table/export', 'export')
+        ->middleware(['auth:sanctum', 'role:' . $role::COMMISSION . '|' . $role::ORGANIZATION .'|' .$role::ADMIN.'|'.$role::ROOT])
+        ->name('appointment_race.export');
 });
 
 Route::controller(App\Http\Controllers\Api\LocationController::class)->group(function () {
