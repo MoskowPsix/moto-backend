e233c855e4951b133d917ae6c27fb6b66a224e39
Makswer <makswolk6@yandex.ru>
2025-05-16 14:08:15 +0300

    add: generateLinkMethod in PaymentService.php

--- app/Services/Payment/PaymentService.php
+++ app/Services/Payment/PaymentService.php
@@ -43,4 +43,45 @@ class PaymentService implements PaymentServiceContract
         ]);
         return "https://auth.robokassa.ru/Merchant/Index.aspx?" . $path;
     }
+    public function generateLinkWithSaveCard(Transaction $transaction, string $opKey): string
+    {
+        $attendance = $transaction->attendances()->first();
+
+        $store = $attendance->track()->first()->store()->first();
+        $login = $store->login;
+        $password = $store->password_1;
+
+        $attendances = $transaction->attendances;
+        $outSum = $attendances->sum(fn($a) => $a->price);
+        $invoiceId = $transaction->id;
+
+        $receipt = [
+            'sno' => 'usn_income',
+            'items' => [],
+        ];
+        foreach ($attendances as $attendance) {
+            $receipt['items'][] = [
+                'name'     => $attendance->name,
+                'quantity' => 1,
+                'sum'      => $attendance->price,
+                'tax'      => $attendance->tax ?? 'none',
+            ];
+        }
+        $jsonString = json_encode($receipt, JSON_UNESCAPED_UNICODE);
+        $encodedReceipt = urlencode($jsonString);
+
+        $crcString = "$login:$outSum:$invoiceId:$encodedReceipt:$opKey:$password";
+        $signature = md5($crcString);
+
+        $params = http_build_query([
+            'MerchantLogin'  => $login,
+            'OutSum'         => $outSum,
+            'InvId'          => $invoiceId,
+            'SignatureValue' => $signature,
+            'Receipt'        => $encodedReceipt,
+            'Token'          => $opKey,
+        ]);
+
+        return "https://auth.robokassa.kz/Merchant/Payment/CoFPayment?" . $params;
+    }
 }
