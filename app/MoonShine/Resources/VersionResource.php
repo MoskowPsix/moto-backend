ab1c7691cb9111b0ab738208f8557129116fa6d2
MoskowPsix <bolshe.kivi@gmail.com>
2025-05-29 11:55:47 +0500

    add: version control in moonshine

---
+++ app/MoonShine/Resources/VersionResource.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Version;
+
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\UI\Components\Layout\Box;
+use MoonShine\UI\Fields\ID;
+use MoonShine\Contracts\UI\FieldContract;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Fields\Text;
+
+/**
+ * @extends ModelResource<Version>
+ */
+class VersionResource extends ModelResource
+{
+    protected string $model = Version::class;
+
+    protected string $title = 'Версии';
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function indexFields(): iterable
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Номер', 'version_number')->sortable()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract|FieldContract>
+     */
+    protected function formFields(): iterable
+    {
+        return [
+            Box::make([
+                ID::make(),
+                Text::make('Номер', 'version_number')
+            ])
+        ];
+    }
+
+    /**
+     * @return list<FieldContract>
+     */
+    protected function detailFields(): iterable
+    {
+        return [
+            ID::make(),
+            Text::make('Номер', 'version_number')
+        ];
+    }
+
+    /**
+     * @param Version $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    protected function rules(mixed $item): array
+    {
+        return [];
+    }
+}
